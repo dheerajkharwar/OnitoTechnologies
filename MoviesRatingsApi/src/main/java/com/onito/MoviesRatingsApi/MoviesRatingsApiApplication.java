6e97d6e9be5deac678dd48079a031ec1bb608173
dheerajkharwar <dheerajkharwar9@gmail.com>
2023-05-29 21:04:36 -0700

    Spring Starter Project Created

---
+++ MoviesRatingsApi/src/main/java/com/onito/MoviesRatingsApi/MoviesRatingsApiApplication.java
@@ -0,0 +1,13 @@
+package com.onito.MoviesRatingsApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesRatingsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesRatingsApiApplication.class, args);
+	}
+
+}
